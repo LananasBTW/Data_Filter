cf2eb56bbe9b4763d6d651224218811db83c5fd5
Ptitmorceaux <106253875+ptitmorceaux@users.noreply.github.com>
2025-11-28 18:46:59 +0100

    Add CSV data file and refactor CSV and JSON handling functions for improved data conversion

--- modules/formats/fcsv.py
+++ modules/formats/fcsv.py
@@ -1,29 +1,45 @@
+import os
 import csv
 import json
+import config
 
-def convert_value(value):
+def convert_strToValue(value):
     value = value.strip()
-
     try:
         value = json.loads(value)
     except json.JSONDecodeError:
         pass
     return value
 
-def load(path):    
+def convert_valueToStr(value):
+    if isinstance(value, (list, dict)):
+        return json.dumps(value)
+    return str(value)
+
+def load(path):
     with open(path, 'r') as f:
         reader = csv.DictReader(f)
         data = [row for row in reader]
     
     for row in data:
         for k, v in row.items():
-            row[k] = convert_value(v)
+            row[k] = convert_strToValue(v)
 
     return data
 
+def save(data, filename):
+    tmp_data = data.copy()
+    for row in tmp_data:
+        for k, v in row.items():
+            row[k] = convert_valueToStr(v)
+        
+    tmp_path = os.path.join(config.TMP_DIR, filename + ".csv")
+    path = os.path.join(config.OUTPUT_DIR, filename + ".csv")
 
-def save(data, path):    
-    with open(path, 'w', newline='') as f:
-        writer = csv.DictWriter(f, fieldnames=data[0].keys())
+    with open(tmp_path, 'w', newline='') as f:
+        writer = csv.DictWriter(f, fieldnames=tmp_data[0].keys())
         writer.writeheader()
-        writer.writerows(data)
+        writer.writerows(tmp_data)
+        
+    os.replace(tmp_path, path)
+    return path
--- modules/formats/fjson.py
+++ modules/formats/fjson.py
@@ -1,11 +1,17 @@
+import os
 import json
-
+import config
 
 def load(path):    
     with open(path, 'r') as f:
         return json.load(f)
 
+def save(data, filename, indent=4):
+    tmp_path = os.path.join(config.TMP_DIR, filename + ".json")
+    path = os.path.join(config.OUTPUT_DIR, filename + ".json")
 
-def save(data, path, indent=4):
-    with open(path, 'w') as f:
+    with open(tmp_path, 'w') as f:
         json.dump(data, f, indent=indent)
+
+    os.replace(tmp_path, path)
+    return path
