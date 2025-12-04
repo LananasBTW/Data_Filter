e8a895e341f6502e30f5822301d4919bdc7916e7
Ptitmorceaux <106253875+ptitmorceaux@users.noreply.github.com>
2025-12-04 17:59:23 +0100

    Update .gitignore and config paths for data organization

--- config.py
+++ config.py
@@ -4,7 +4,9 @@ import os
 CURRENT_PATH = os.path.dirname(os.path.abspath(__file__))
 
 TAB_PADDING = 2
-OUTPUT_DIR = os.path.join(CURRENT_PATH, "output/")
+
+DATA_DIR = os.path.join(CURRENT_PATH, "data/")
+OUTPUT_DIR = os.path.join(DATA_DIR, "output/")
 TMP_DIR = os.path.join(CURRENT_PATH, "tmp/")
 
 STATS_MAX_SAMPLE_VALUES = 3
